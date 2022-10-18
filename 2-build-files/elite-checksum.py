cdf44a2832ff12eff3dc3f5870e8d5e308a49780
Mark Moxon <mark@moxon.net>
2022-10-18 15:15:08 +0100

    Move stuff around

--- 2-build-files/elite-checksum.py
+++ 2-build-files/elite-checksum.py
@@ -40,7 +40,7 @@ scramble_from = 0x2cc1
 
 if release == 1:
     # SNG47
-    scramble_to = 0x7FB0    # Modified for Universe Editor to F% - 1
+    scramble_to = 0x7FC9    # Modified for Universe Editor to F% - 1
 elif release == 2:
     # Compact
     scramble_to = 0x7FEC
